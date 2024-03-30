1ea9835cd7afc56bed0318b8826c3c9261760047
Yasanjith Rajapathirane <125336683+YasanjithR@users.noreply.github.com>
2024-03-30 08:44:37 +0530

    route update

--- frontend/src/components/NoteCard.js
+++ frontend/src/components/NoteCard.js
@@ -8,7 +8,7 @@ const NoteCard = ({ onClick }) => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const response = await axios.get("http://localhost:8070/note");
+        const response = await axios.get("https://notify-backends.vercel.app/note");
         setNotes(response.data);
       } catch (error) {
         console.error("Error fetching notes:", error);
